d623bc6f4137af6200826c4edfd94bf933a81054
Michael <Michael.Fornaro@anz.com>
2019-03-25 16:36:17 +1100

    updating version to 0.1.2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "proxy"
-	app.Version = "0.1.0"
+	app.Version = "0.1.2"
 	app.Usage = "executing and configuring cntlm"
 	app.Authors = []cli.Author{
 		{
