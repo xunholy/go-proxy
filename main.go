0e5224209b02ff2931dc064a89d6684d68669e1f
Michael <Michael.Fornaro@anz.com>
2019-03-29 15:56:11 +1100

    bump version

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "proxy"
-	app.Version = "0.1.2"
+	app.Version = "0.1.3"
 	app.Usage = "executing and configuring cntlm"
 	app.Authors = []cli.Author{
 		{
