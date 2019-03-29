07f160337be2befdc05a0f828ad3fc138ab9acc4
Michael <Michael.Fornaro@anz.com>
2019-03-29 11:16:34 +1100

    fixing bug with improvements

--- internal/cmd/stop.go
+++ internal/cmd/stop.go
@@ -12,18 +12,10 @@ import (
 
 func StopCommand() cli.Command {
 	return cli.Command{
-
 		Name:        "stop",
 		Aliases:     []string{""},
 		Usage:       "proxy stop",
 		Description: "Stop CNTLM Proxy",
-		Flags: []cli.Flag{
-			cli.BoolFlag{
-				Name:        "all, a",
-				Usage:       "unset all CNTLM config",
-				Destination: &setAll,
-			},
-		},
 		Action: func(_ *cli.Context) {
 			profile.UpdateGlobalEnvironmentVariables("")
 			cmds := execute.Command{Cmd: "pkill", Args: []string{"cntlm"}}
